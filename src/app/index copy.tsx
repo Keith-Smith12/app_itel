feb6d4dd34d82b3ae6870bfcdafa5378a2a3a28e
Magnara-Campos <magnaracampos@gmail.com>
2025-06-02 10:58:19 -0300

    push 02.06.25

---
+++ src/app/index copy.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'expo-router';
+import { StyleSheet, Text, TouchableOpacity, View,Image } from 'react-native';
+
+export default function WelcomePage() {
+  return (
+    <View style={styles.container}>
+     <Image
+      source={require('../../assets/images/ITEL_Logo.png')}
+    />
+     {/* <Text style={styles.title}>Bem-vindo!</Text>
+      <Link href="/login" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </TouchableOpacity>
+      </Link>
+      */}
+    </View>
+
+      
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+}); 
